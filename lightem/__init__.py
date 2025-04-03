faef4d0eb85a42c28cf449a35e6d6d3a33c8ad9c
Pedro Pampolini <pedromendicino25@gmail.com>
2025-04-03 19:54:03 -0300

    feat: versioning file added

--- lightem/__init__.py
+++ lightem/__init__.py
@@ -3,3 +3,4 @@ from .Embedder import Embedder, EmbedderTypes
 from .Matcher import Matcher, MatcherTypes
 from .Clusterer import Clusterer
 from .Table import Table, TableManager
+from .version import __version__, __author__, __url__
--- setup.py
+++ setup.py
@@ -2,36 +2,49 @@ from setuptools import setup, find_packages
 import os
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+  long_description = fh.read()
+
+def getPackageInfo():
+  about = {}
+  with open(os.path.join("lightem", "version.py"), "r") as f:
+    exec(f.read(), about)
+  return about
+
+about = getPackageInfo()
+
+# pede permissão de adm pra apagar as pastas build, dist e lightem.egg-info
+os.system("rmdir /S /Q build")
+os.system("rmdir /S /Q dist")
+os.system("rmdir /S /Q lightem.egg-info")
 
 setup(
-    name="lightem",
-    version="0.1.3",
-    description="A light-weight python package for multi-table entity matching.",
-    packages=find_packages(),  # Ajuste aqui
-    package_dir={},  # Ajuste para o diretório 'lightem'
-    include_package_data=False,
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/PedroPampolini/lightEM",
-    author="Pedro Pampolini Mendicino",
-    author_email="<pedromendicino25@gmail.com>",
-    license="MIT",
-    classifiers=[
-        "Development Status :: 1 - Planning",
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=[
-        "pandas",
-        "numpy",
-        "scikit-learn",
-        "gensim",
-        "sentence-transformers",
-    ],
-    extras_require={
-        "dev": ["twine>=6.1.0",],
-    },
-    python_requires='>=3.10',
+  name="lightem",
+  version=about["__version__"],
+  description="A light-weight python package for multi-table entity matching.",
+  packages=find_packages(),  # Ajuste aqui
+  package_dir={},  # Ajuste para o diretório 'lightem'
+  include_package_data=False,
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url=about["__url__"],
+  author=about["__author__"],
+  author_email="<pedromendicino25@gmail.com>",
+  license="MIT",
+  classifiers=[
+    "Development Status :: 1 - Planning",
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  install_requires=[
+    "pandas",
+    "numpy",
+    "scikit-learn",
+    "gensim",
+    "sentence-transformers",
+  ],
+  extras_require={
+    "dev": ["twine>=6.1.0",],
+  },
+  python_requires='>=3.10',
 )
